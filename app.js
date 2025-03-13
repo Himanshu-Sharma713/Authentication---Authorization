8f7d71f9d699f7c04484c85b02a3a9ca8d461a3f
Himanshu-Sharma713 <himanshusb713@gmail.com>
2025-03-13 15:53:49 +0530

    bcrypt - for encryption

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const cookieParser = require("cookie-parser");
 const express = require("express");
+const bcrypt = require("bcrypt");
 
 const app = express();
 const PORT = 3002;
@@ -7,13 +8,11 @@ const PORT = 3002;
 app.use(cookieParser());
 
 app.get("/", (req, res) => {
-  res.cookie("name", "Himanshu");
-  res.send("Done");
-});
-
-app.get("/read", (req, res) => {
-  console.log(req.cookies);
-  res.send("Read Page");
+  bcrypt.genSalt(10, function (err, salt) {
+    bcrypt.hash("pololololoo", salt, function (err, hash) {
+      console.log(hash);
+    });
+  });
 });
 
 app.listen(PORT, () => {
